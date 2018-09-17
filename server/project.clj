e5163509a60a2ddffa44e1ea845ff0b4c7e26a0d
Coen Stange <coenstange@me.com>
2018-09-17 18:12:35 +0200

    + token request

--- server/project.clj
+++ server/project.clj
@@ -1,7 +1,9 @@
 (defproject server "0.1.0-SNAPSHOT"
   :description "Funda crawler server"
   :url "https://github.com/coens97/funda-pricemap"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [http-kit "2.2.0"]
+                 [danlentz/clj-uuid "0.1.7"]]
   :aot [server.TopologySubmitter]
   ;; include storm dependency only in dev because production storm cluster provides it
   :profiles {:dev {:dependencies [[storm "0.8.1"]]}})
--- server/src/server/spouts.clj
+++ server/src/server/spouts.clj
@@ -4,17 +4,30 @@
 More info on the Clojure DSL here:
 
 https://github.com/nathanmarz/storm/wiki/Clojure-DSL"
-  (:require [backtype.storm [clojure :refer [defspout spout emit-spout!]]]))
+  (:require [clj-uuid :as uuid]
+            [org.httpkit.client :as http]
+            [backtype.storm [clojure :refer [defspout spout emit-spout!]]]))
 
 (defspout type-spout ["type"]
   [conf context collector]
   (let [stormys [:regular :bizarro]]
     (spout
      (nextTuple []
-       (Thread/sleep 1000)
-       (emit-spout! collector [(rand-nth stormys)]))
+                (http/get
+                 (str "https://mobile.funda.io/api/v1/Token/" (uuid/v1))
+                 {:user-agent "Funda/74 CFNetwork/902.2 Darwin/17.7.0"
+                  :headers {"Cookie" "X-Stored-Data=null"
+                                  ;; "User-Agent" "Funda/74 CFNetwork/902.2 Darwin/17.7.0"
+                            "Accept-Language" "nl-NL"}}
+                 (fn [{:keys [status headers body error]}] ;; asynchronous response handling
+                   (println body)
+                   (if error
+                     (println "Failed, exception is " error)
+                     (println "Async HTTP GET: " status))))
+                (emit-spout! collector [(rand-nth stormys)])
+                (Thread/sleep (* 1000 60 60 24)))
      (ack [id]
         ;; You only need to define this method for reliable spouts
         ;; (such as one that reads off of a queue like Kestrel)
         ;; This is an unreliable spout, so it does nothing here
-        ))))
+))))
