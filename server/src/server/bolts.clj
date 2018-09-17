141db184dc7ba2c207515ef355f786467fb6a18e
Coen Stange <coenstange@me.com>
2018-09-17 21:35:34 +0200

    + add houselist request

--- server/src/server/bolts.clj
+++ server/src/server/bolts.clj
@@ -4,7 +4,8 @@
 More info on the Clojure DSL here:
 
 https://github.com/nathanmarz/storm/wiki/Clojure-DSL"
-  (:require [backtype.storm [clojure :refer [emit-bolt! defbolt ack! bolt]]]))
+  (:require [org.httpkit.client :as http]
+            [backtype.storm [clojure :refer [emit-bolt! defbolt ack! bolt]]]))
 
 (defbolt stormy-bolt ["stormy"] [{type :type :as tuple} collector]
   (emit-bolt! collector [(case type
@@ -15,5 +16,22 @@ https://github.com/nathanmarz/storm/wiki/Clojure-DSL"
   (ack! collector tuple))
 
 (defbolt server-bolt ["message"] [{stormy :stormy :as tuple} collector]
-  (emit-bolt! collector [(str "server produced: "stormy)] :anchor tuple)
+  (emit-bolt! collector [(str "server produced: " stormy)] :anchor tuple)
+  (ack! collector tuple))
+
+(defbolt houselist-bolt ["id"] [{token :token :as tuple} collector]
+  ;;(println token)
+  (http/get
+   (str "https://mobile.funda.io/api/v1/Aanbod/koop/heel-nederland%252Fbeschikbaar%252F?page=" 1 "&pageSize=25&compact=True")
+   {:user-agent "Funda/74 CFNetwork/902.2 Darwin/17.7.0" ;; Impersonate iPhone App
+    :headers {"Accept" "*/*"
+              "Cookie" "X-Stored-Data=null"
+              "Accept-Language" "nl-NL"
+              "api_key" token}}
+   (fn [{:keys [status headers body error]}] ;; asynchronous response handling
+     (if error
+            ;; Failed
+       (println "Failed, exception is " error)
+           ;; Success
+       (do (println body)))))
   (ack! collector tuple))
--- server/src/server/topology.clj
+++ server/src/server/topology.clj
@@ -5,17 +5,18 @@ More info on the Clojure DSL here:
 
 https://github.com/nathanmarz/storm/wiki/Clojure-DSL"
   (:require [server
-             [spouts :refer [type-spout]]
-             [bolts :refer [stormy-bolt server-bolt]]]
+             [spouts :refer [token-spout]]
+             [bolts :refer [stormy-bolt server-bolt houselist-bolt]]]
             [backtype.storm [clojure :refer [topology spout-spec bolt-spec]] [config :refer :all]])
   (:import [backtype.storm LocalCluster LocalDRPC]))
 
 (defn stormy-topology []
   (topology
-   {"spout" (spout-spec type-spout)}
+   {"spout" (spout-spec token-spout)}
 
-   {"stormy-bolt" (bolt-spec {"spout" ["type"]} stormy-bolt :p 2)
-    "server-bolt" (bolt-spec {"stormy-bolt" :shuffle} server-bolt :p 2)}))
+   ;;{"stormy-bolt" (bolt-spec {"spout" ["type"]} stormy-bolt :p 2)
+   ;; "server-bolt" (bolt-spec {"stormy-bolt" :shuffle} server-bolt :p 2)}))
+   {"houselist-bolt" (bolt-spec {"spout" ["token"]} houselist-bolt :p 1)}))
 
 (defn run! [& {debug "debug" workers "workers" :or {debug "true" workers "2"}}]
   (doto (LocalCluster.)
