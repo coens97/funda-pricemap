4ecd11df243faf72df8f053396c1cd38bf018d5b
Coen Stange <coenstange@me.com>
2018-10-21 21:27:43 +0200

    * fix fix fix

--- server/src/server/coreApp.clj
+++ server/src/server/coreApp.clj
@@ -42,7 +42,7 @@
     (if-not (.exists (io/as-file filename))
       (let [token (get-token)
             result (->
-                    (f/parallelize sc (range 1 ((nr-of-pages token)))
+                    (f/parallelize sc (range 1 (nr-of-pages token)))
                     ;; Go through the list of houses available
                     (f/flat-map (f/iterator-fn [page] (house-ids token page)))
                     ;; Retrieve each house
@@ -116,7 +116,7 @@
   "Entry point"
   []
   (def c (-> (conf/spark-conf)
-             (conf/master "local[1]")
+             (conf/master "local[*]")
              (conf/app-name "funda-crawler")))
 
   (def sc (f/spark-context c))
